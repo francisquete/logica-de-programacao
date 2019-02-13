af2b8667ab0609889348b100e77dbcff1c0a91b6
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-02-12 23:45:03 -0200

    conceitos iniciais de lista

---
+++ estrutura-de-dados/lista.py
@@ -0,0 +1,12 @@
+lista = [1, 2, 7, 4, 6, 9]
+print(type(lista))
+
+print(lista[1]+lista[0])
+
+l = ['a', 'b', 'c', 'd']
+print(l[0])
+
+lista = list(('masanori'))
+print(lista)
+aaa = ['sas', 'sadfrfe', 1]
+print(aaa)
